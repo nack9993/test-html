7425638a681c4f8326a9f63043ae64655708b644
nack9993 <nack99993@gmail.com>
2022-04-25 18:43:09 -0700

    Update the left right arrow

--- js/summary.js
+++ js/summary.js
@@ -2,8 +2,8 @@ const leftArrow = document.getElementById("left-arrow");
 const rightArrow = document.getElementById("right-arrow");
 const enter = document.getElementById("enter");
 
-leftArrow.addEventListener("click", () => changeCard());
-rightArrow.addEventListener("click", () => changeCard());
+leftArrow.addEventListener("click", () => changeCard("left"));
+rightArrow.addEventListener("click", () => changeCard("right"));
 
 const cardList = ["n", "m", "p"];
 let currentCard = 0;
@@ -12,24 +12,44 @@ enter.addEventListener("click", () => {
   const inputPassword = document.getElementById("password");
   console.log(inputPassword.value);
 
-  const card = document.getElementById("card");
-  card.src = `/assets/${cardList[currentCard]}-charm.png`;
+  if (inputPassword.value === cardList[currentCard]) {
+    const card = document.getElementById("card");
+    card.src = `/assets/${cardList[currentCard]}-charm.png`;
 
-  const actions = document.getElementById("actions");
-  actions.classList.add("disabled");
+    const actions = document.getElementById("actions");
+    actions.classList.add("disabled");
+  } else {
+    alert("NOPE");
+  }
 });
 
-function changeCard() {
-  if (currentCard < cardList.length - 1) {
-    currentCard = currentCard + 1;
-  } else {
-    currentCard = 0;
+function changeCard(direction) {
+  if (direction === "right") {
+    if (currentCard < cardList.length - 1) {
+      currentCard = currentCard + 1;
+    } else {
+      currentCard = 0;
+    }
   }
+
+  if (direction === "left") {
+    if (currentCard == 0) {
+      currentCard = 2;
+    } else if (currentCard == 2) {
+      currentCard = 1;
+    } else if (currentCard == 1) {
+      currentCard = 0;
+    }
+  }
+
   const card = document.getElementById("card");
   card.src = `/assets/${cardList[currentCard]}-key.png`;
 
   const inputPassword = document.getElementById("password");
   inputPassword.setAttribute("key", cardList[currentCard]);
+
+  const actions = document.getElementById("actions");
+  actions.classList.remove("disabled");
   console.log(inputPassword.getAttribute("key"));
 }
 
@@ -40,6 +60,6 @@ $(document).ready(function () {
   });
 
   $("#contact").click(() => {
-    window.location.href = "contact.html";
+    window.location.href = "Contact.html";
   });
 });
